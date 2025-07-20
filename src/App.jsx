8f59b4f867457f5d3be2036ea767dbb01dfcf73d
Jakir Hussain <jakirsks646@gmail.com>
2025-07-20 12:09:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Skills from './assets/components/skills.jsx';
+
 import Loading from './assets/components/loading.jsx';
 import Home from './assets/components/home.jsx';
 import Projects from './assets/components/projects.jsx';
@@ -22,8 +22,7 @@ function App() {
           <Route path="/" element={loaded ? <Home /> : <Loading />} />
           {/* Add other routes here as needed */}
           <Route path="/projects" element={<Projects />} />
-          {/* Add other routes here as needed */}
-          <Route path="/skills" element={<Skills/>}></Route>
+          
         </Routes>
       </Router>
     </>
